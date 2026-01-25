f4584c75e37b9d2f7fac4d02bb9d37af68a75262
Timothy Gillenwater <tgillenwater@Mac.attlocal.net>
2026-01-24 23:32:02 -0600

    dashboard ui&ux

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = function verifyToken(req, res, next) {
+module.exports = function (req, res, next) {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
