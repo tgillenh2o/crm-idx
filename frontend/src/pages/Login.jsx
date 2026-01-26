8379154910571813ffb068eefa56060b8c8b68e0
Timothy Gillenwater <tgillenwater@Mac.attlocal.net>
2026-01-25 22:10:32 -0600

    dashboard ui&ux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import "./Auth.css";
+
 
 export default function Login() {
   const [email, setEmail] = useState("");
