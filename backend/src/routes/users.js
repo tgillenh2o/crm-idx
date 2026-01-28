c2cdf9bb936cbfcebbeb8219687ba4aa60afa3d2
Timothy Gillenwater <tgillenwater@Mac.attlocal.net>
2026-01-28 12:20:40 -0600

    dashboard ui&ux

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
+const verifyToken = require("../middleware/auth");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
